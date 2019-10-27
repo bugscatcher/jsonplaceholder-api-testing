e89ac12d4bd243f710157a78c6186c403935dbb1
bugscatcher <inurrick@gmail.com>
2019-10-27 21:49:32 +0300

    Rename test and replace loop with stream

--- src/test/java/com/github/bugscatcher/Tests.java
+++ src/test/java/com/github/bugscatcher/Tests.java
@@ -6,11 +6,13 @@ import com.github.bugscatcher.dto.UserDTO;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static com.github.bugscatcher.TestUtil.*;
 
 public class Tests extends Abstract {
     @Test
-    public void checkEmails() {
+    public void checkEmailsFormat() {
         String property = "username";
         String username = properties.getProperty(property);
         Assert.assertNotNull(getMessageForNonExistentProperty(property), username);
@@ -19,12 +21,14 @@ public class Tests extends Abstract {
         Assert.assertNotNull(getMessageForNonExistentUser(username), user);
 
         PostDTO[] posts = getResource(EndPoints.POSTS + "?userId=" + user.getId(), PostDTO[].class);
-        for (PostDTO post : posts) {
-            CommentsDTO[] comments = getResource(EndPoints.COMMENTS + "?postId=" + post.getId(), CommentsDTO[].class);
-            for (CommentsDTO comment : comments) {
-                boolean isValid = TestUtil.isValid(comment.getEmail());
-                Assert.assertTrue(getMessageForEmailInvalidFormat(comment.getEmail()), isValid);
-            }
-        }
+        Arrays.stream(posts)
+                .forEach(post -> {
+                    CommentsDTO[] comments = getResource(EndPoints.COMMENTS + "?postId=" + post.getId(), CommentsDTO[].class);
+                    Arrays.stream(comments)
+                            .forEach(comment -> {
+                                boolean isValid = TestUtil.isValid(comment.getEmail());
+                                Assert.assertTrue(getMessageForEmailInvalidFormat(comment.getEmail()), isValid);
+                            });
+                });
     }
 }
