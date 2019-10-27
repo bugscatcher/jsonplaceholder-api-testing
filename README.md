f3c090731515e0dd3b2ed6b155dfbe7d444bc421
bugscatcher <inurrick@gmail.com>
2019-10-27 21:37:23 +0300

    Add Lombok dependency

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # jsonplaceholder-api-testing
+
+## How to run tests: ##
+`./gradlew clean test`
+
+## Using Lombok with IntelliJ IDEA: ##
+ 1. [Install the Lombok Plugin ](https://projectlombok.org/setup/intellij)
+ 1. Enable annotation processing
+    1. Go to Preferences... > Build, Execution, Deployment > Compiler > Annotation Processors
+    1. Set check box: 
+       - [x] Enable annotation processing
+    1. After enabling, run Build -> Rebuild Project 
--- src/test/java/com/github/bugscatcher/dto/UserDTO.java
+++ src/test/java/com/github/bugscatcher/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package com.github.bugscatcher.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
     private int id;
     private String name;
@@ -9,69 +12,4 @@ public class UserDTO {
     private String phone;
     private String website;
     private CompanyDTO company;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public AddressDTO getAddress() {
-        return address;
-    }
-
-    public void setAddress(AddressDTO address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public CompanyDTO getCompany() {
-        return company;
-    }
-
-    public void setCompany(CompanyDTO company) {
-        this.company = company;
-    }
 }
-
