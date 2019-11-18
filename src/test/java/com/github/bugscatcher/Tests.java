e2a42d79f2aa3a2c593f2a25cd14a8e1a3981a2a
bugscatcher <inurrick@gmail.com>
2019-11-18 15:01:26 +0800

    Add negative case

--- src/test/java/com/github/bugscatcher/Tests.java
+++ src/test/java/com/github/bugscatcher/Tests.java
@@ -15,30 +15,60 @@ public class Tests extends Abstract {
     public void checkEmailsFormat() {
         String property = "username";
         String username = properties.getProperty(property);
-//        ... but this does not take into account the fact that we can store several usernames.
-//        In this case, we need to read the properties something like `properties.getProperty(property).split(",")`
-//        and check user's post in loop.
         Assert.assertNotNull(getMessageForNonExistentProperty(property), username);
 
         UserDTO user = searchUser(username);
         Assert.assertNotNull(getMessageForNonExistentUser(username), user);
 
-        PostDTO[] posts = getResource(EndPoints.POSTS + user.getId(), PostDTO[].class);
-        if (posts.length == 0) {
-            LOG.warn("No posts for userId=" + user.getId());
-        }
+        PostDTO[] posts = getPosts(user.getId());
+        Assert.assertNotEquals(0, posts.length);
 
         Arrays.stream(posts)
                 .forEach(post -> {
-                    CommentsDTO[] comments = getResource(EndPoints.COMMENTS + post.getId(), CommentsDTO[].class);
-                    if (comments.length == 0) {
-                        LOG.warn("No comments for postId=" + post.getId());
-                    }
+                    CommentsDTO[] comments = getComments(post.getId());
                     Arrays.stream(comments)
                             .forEach(comment -> {
                                 boolean isValid = TestUtil.isValid(comment.getEmail());
                                 Assert.assertTrue(getMessageForEmailInvalidFormat(comment.getEmail()), isValid);
                             });
                 });
+    }
+
+    @Test
+    public void checkEmailsFormat_negative() {
+//        Here, I'm using ready-made data from db.json. This can be replaced by creating a user, post and comment through requests.
+        String property = "username.negative";
+        String username = properties.getProperty(property);
+        Assert.assertNotNull(getMessageForNonExistentProperty(property), username);
+
+        UserDTO user = searchUser(username);
+        PostDTO[] posts = getPosts(user.getId());
+        Assert.assertNotEquals(0, posts.length);
+
+        Arrays.stream(posts)
+                .forEach(post -> {
+                    CommentsDTO[] comments = getComments(post.getId());
+                    Arrays.stream(comments)
+                            .forEach(comment -> {
+                                boolean isValid = TestUtil.isValid(comment.getEmail());
+                                Assert.assertTrue(getMessageForEmailInvalidFormat(comment.getEmail()), isValid);
+                            });
+                });
+    }
+
+    private PostDTO[] getPosts(int userID) {
+        PostDTO[] posts = getResource(EndPoints.POSTS + userID, PostDTO[].class);
+        if (posts.length == 0) {
+            LOG.warn("No posts for userId=" + userID);
+        }
+        return posts;
+    }
+
+    private CommentsDTO[] getComments(int postID) {
+        CommentsDTO[] comments = getResource(EndPoints.COMMENTS + postID, CommentsDTO[].class);
+        if (comments.length == 0) {
+            LOG.warn("No comments for postId=" + postID);
+        }
+        return comments;
     }
 }
